6dbefa39d91a23c379a7f3dfe3c722c278198e8f
Arlis Felipe Sanchez Buitrago <60239223+afsanchezb@users.noreply.github.com>
2020-10-22 18:01:32 -0500

    Add files via upload

---
+++ tallerTypescript/scripts/dataCourses.js
@@ -0,0 +1,10 @@
+import { Course } from './Course.js';
+export var dataCourses = [
+    new Course("Desarrollo de sw", "Ruby Casallas", 3),
+    new Course("Aleman 2", "Claudia Vargas", 3),
+    new Course("Algebra Lineal", "Carolina Benedetti", 3),
+    new Course("Infraestructura computacional", "Harold", 3),
+    new Course("Lenguajes y Maquinas(I)", "Silvia Takahashi", 3),
+    new Course("Tecnologia de Ironman", "Johann Osma", 2),
+    new Course("TI en las Organizaciones", "Guillermo Beltran", 3)
+];
